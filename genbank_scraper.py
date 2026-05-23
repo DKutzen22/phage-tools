4f6e2396854d89a748e5eb8201694f914dccc0a9
dkutzen22 <daltonkkutzen@gmail.com>
2026-05-23 20:57:09 +0000

    updated comments and reorganized functions

--- genbank_scraper.py
+++ genbank_scraper.py
@@ -2,15 +2,13 @@ from Bio import Entrez, SeqIO
 import xlsxwriter
 import re
 
+#sets variables for fetching data from NCBI GenBank
 Entrez.email = input("What is your NCBI email?")
 Entrez.tool = "genbank_phage_genome"
 genbank_id = input("What is the Phages Genbank ID Number?")
 
-
-
 #retrieves genome data from NCBI GenBank
 def genome_scraping(accession_num):
-
     #fetches the genbank data from the nucleotide database for the provided 'accession_num'
     with Entrez.efetch(db="nucleotide", 
                         id=accession_num, 
@@ -21,42 +19,39 @@ def genome_scraping(accession_num):
 
     return record
 
-
-#organizes the reference genome into a dictionary with relevant features
+#organizes the reference genome into a dictionary of proteins
 def protein_scraping(accession_num):
-
+    #retrieves the referenced genome
+    #then creates a dict and additionally a protein counter for verification
     genome = genome_scraping(accession_num)
     proteins = {}
-    #counter to verify the amount of proteins in the finished dictionary is correct
     cds_count = 0
-
+    #parses the seqrecord features, fetching proteins and ignoring any others
     for feature in genome.features:
         #checks that the feature is a protein coding sequence
         if feature.type != "CDS": continue
         cds_count += 1
-        #labels the feature, its name, and sequence
+        #variables for protein name, product, and translation
         gp = feature.qualifiers.get("note", "blank")[0].split(";")[0]
         product = feature.qualifiers.get("product", "blank")[0]
         translation = feature.qualifiers.get("translation", "blank")[0]
-        #if the feature doesn't have a note, it labels with locus_tag instead
+        #if the feature doesn't have a note, its named with locus_tag instead
         if gp == "b":
             gp = feature.qualifiers.get("locus_tag")[0]
-        #places the feature in the 'proteins' dictionary with its name and sequence 
-        #organized by 'gp' to avoid duplication of keys
+        #places the feature in the 'proteins' dictionary with its product and translation 
+        #organized by name to avoid duplication of keys
         proteins[gp] = {"Product":product, "Translation":translation}
         if cds_count != len(proteins):
             print("Error: Not all proteins were sorted correctly")
 
     return proteins
 
-
-
-#create a function to retrieve all general data about the organism
+#retrieves general organism data from the genome
 def organism_scraping(accession_num):
-
+    #retrieves the genome and creates a filler re.pattern for 'Description'
     genome = genome_scraping(accession_num)
     Description_pattern = re.compile(r"supercalifragilisticexpialidocious")
-
+    #dict for various relevant facts about the organism
     description = {
         "pattern": Description_pattern,
         "Organism": {},
@@ -65,7 +60,7 @@ def organism_scraping(accession_num):
         "Identified by": {},
         "Location": {}
     }
-
+    #retrieves the organism data and adds it to the 'description' dict
     for feature in genome.features:
         if feature.type == "source":
             description["Organism"]["Product"] = feature.qualifiers.get("organism", "blank")[0]
@@ -76,14 +71,12 @@ def organism_scraping(accession_num):
 
     return description
 
-
-
-#create a function that organizes the proteins by type
+#organizes the proteins into a dict by class
 def protein_sorting(accession_num):
-
+    #retrieves proteins and organism data for sorting
     proteins = protein_scraping(accession_num)
     organism_data = organism_scraping(accession_num)
-
+    #re.patterns for sorting of proteins into classes
     Capsid_pattern = re.compile(r"\bhead\b|\bcapsid\b|\bcoat\b")
     Tail_pattern = re.compile(r"tail")
     Lysin_pattern = re.compile(r"lysin|lysozyme")
@@ -93,8 +86,8 @@ def protein_sorting(accession_num):
     Integrase_pattern = re.compile(r"integrase")
     Others_pattern = re.compile(r"superinfection|assembly|prohead|connector|portal")
     Hypothetical_pattern = re.compile(r"\bhypothetical\sprotein\b")
-
-    #nested dictionary for sorting proteins into various classes
+    #nested dict with various classes of 
+    #relevant proteins with included re.patterns
     sorted_proteins = {
         "Description": organism_data,
         "Capsid": {"pattern": Capsid_pattern},
@@ -107,14 +100,14 @@ def protein_sorting(accession_num):
         "Others": {"pattern": Others_pattern},
         "Hypothetical": {"pattern": Hypothetical_pattern},
     }
-
     #iterates through proteins for sorting
     for protein in proteins:
-        #retrieves the protein product and translation data stored in 'proteins'
+        #retrieves the protein product and 
+        #translation data stored in 'proteins'
         product = proteins.get(protein).get("Product")
         translation = proteins.get(protein).get("Translation")
         matched = False
-
+        #places proteins in classes with matching re.patterns
         for class_name, sub_dict in reversed(sorted_proteins.items()):
             current_pattern = sub_dict["pattern"]
             if bool(current_pattern.search(product)):
@@ -122,28 +115,27 @@ def protein_sorting(accession_num):
                 matched = True
                 print(f"Matched: {protein} to {class_name}")
                 break
-
+        #puts unmatched proteins into the 'Others' class
         if not matched:
             sorted_proteins["Others"][protein] = {"Product":product, "Translation":translation}
         
     return sorted_proteins
 
-
-
-#make an xlsx with a main info page and various other pages organized by type of protein
+#makes an xlsx with pages for non-empty protein classes, 
+#also includes a 'Description page
 def xlsx_writer(accession_num_x):
-
-    #retrieves the organism data and proteins
+    #retrieves the protein data and then creates an excel workbook
     sorted_proteins = protein_sorting(accession_num_x)
-    #creates an Excel workbook named using the organism name and ID
     workbook = xlsxwriter.Workbook(f"{accession_num_x} - {sorted_proteins.get("Description").get("Organism").get("Product")}.xlsx")
-
+    #iterates through the protein classes, creating worksheets for non-empty classes
     for class_name, protein_class in sorted_proteins.items():
         if len(protein_class) > 1:
             current_worksheet = workbook.add_worksheet(class_name)
             row = 0
             col = 0
-    
+            #iterates through the proteins,
+            #writing their name, product, and translation.
+            #skips the 'patterns' in each protein class
             for protein in protein_class:
                 if protein != "pattern":
                     product = protein_class.get(protein).get("Product")
@@ -156,5 +148,5 @@ def xlsx_writer(accession_num_x):
 
     workbook.close()
 
-
+#runs the last function, which references the others
 xlsx_writer(genbank_id)
